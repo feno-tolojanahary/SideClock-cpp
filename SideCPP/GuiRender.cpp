5d19e02913e7ce421c163f70d31266bd2f6a93af
Feno TOLO <fenoheriniainat@gmail.com>
2023-07-10 22:45:46 +0300

    add: printing content by table

--- SideCPP/GuiRender.cpp
+++ SideCPP/GuiRender.cpp
@@ -1,6 +1,28 @@
 #include "GuiRender.h"
 
-void GuiRender::printListData()
-{
 
+void GuiRender::printListData(vector<vector<string>> contents)
+{
+	auto table = Table(contents);
+	table.SelectAll().Border(LIGHT);
+	// Make first row bold with a double border.
+	table.SelectRow(0).Decorate(bold);
+	table.SelectRow(0).SeparatorVertical(LIGHT);
+	table.SelectRow(0).Border(DOUBLE);
+
+	// Align right the "Release date" column.
+	table.SelectColumn(2).DecorateCells(align_right);
+
+	// Select row from the second to the last.
+	auto content = table.SelectRows(1, -1);
+	// Alternate in between 3 colors.
+	content.DecorateCellsAlternateRow(color(Color::Blue), 3, 0);
+	content.DecorateCellsAlternateRow(color(Color::Cyan), 3, 1);
+	content.DecorateCellsAlternateRow(color(Color::White), 3, 2);
+
+	auto document = table.Render();
+	auto screen = Screen::Create(Dimension::Fit(document));
+	Render(screen, document);
+	screen.Print();
+	std::cout << std::endl;
 }
--- SideCPP/GuiRender.h
+++ SideCPP/GuiRender.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <string>
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/dom/table.hpp"
+#include "ftxui/screen/screen.hpp"
 
 using namespace std;
+using namespace ftxui;
 
 class GuiRender {
 
 public:
 
-	void printListData();
+	void printListData(vector<vector<string>> contents);
 };
