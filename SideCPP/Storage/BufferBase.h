017d4b3a773ea9976aebee1bd1f3219ad05bf175
Feno TOLO <fenoheriniainat@gmail.com>
2023-09-03 21:27:33 +0300

    add internal buffer and bufferbase constructor

--- SideCPP/Storage/BufferBase.h
+++ SideCPP/Storage/BufferBase.h
@@ -21,9 +21,15 @@ public:
 		Position end_pos;
 	};
 
+	BufferBase(Position ptr, size_t size, size_t offset)
+		: pos(ptr + offset), working_buffer(ptr, ptr + size), internal_buffer(ptr, ptr + size) {}
+
 protected:
 
 	size_t bytes = 0;
+
+	/// A reference to a piece of memory for the buffer.
+	Buffer internal_buffer;
 
 	Position pos;
 	Buffer working_buffer;
